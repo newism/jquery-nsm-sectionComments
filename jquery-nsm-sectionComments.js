171982e86d07d17f9c66e611647a12b540dea9cd
Anthony Short <anthony.short@newism.com.au>
2011-08-08 11:29:06 +1000

    Updated the way the content is inserted

--- jquery-nsm-sectionComments.js
+++ jquery-nsm-sectionComments.js
@@ -14,7 +14,7 @@
 				commentTotal: '.comment-total',
 				parentCommentIdInput: 'input[name=parent_id]',
 				sectionIdInput: 'input[name=row_id]',
-				commentReplyTrigger: '> .comment-reply-trigger'
+				commentReplyTrigger: '.comment-reply-trigger'
 				
 			},
 			classes: {
@@ -25,9 +25,6 @@
 
 		// this is an object of all our sections
 		// the key is the section id
-		// the value is a jquery element
-		// each section stores it's comments in .data('comments')
-		// each section stores it's comment count in .data('comment-totals)
 		sections: {},
 		originalComments: false,
 
@@ -35,21 +32,16 @@
 		_create: function () {
 			// Cache the original comments so we don't get duplicates
 			this.originalComments = $(this.options.selectors.comments, this.element);
+			// Store the comment form
+			this.form = this.originalComments.find('#comment-form');
 		},
 
 		// called everytime ‘.comments()’ is executed on an element.
 		_init: function () {
-			
 			// Build the sections
 			this._refresh();
-			
-			// When reply buttons are clicked, it will update the
-			this._initCommentReplyElements(this.originalComments);
-			
-			// Get global comment count
-			this.globalComments = this._getGlobalCommentCount();
-			
-			console.log(this.sections);
+			// Setup reply links for global comments
+			this._setupReplyLinks(0,this.originalComments);
 		},
 		
 		// Refresh the section data
@@ -61,40 +53,39 @@
 				sectionId,
 				$commentTotal,
 				comments,
-				totalComments;
+				totalComments,
+				form;
 
 			for (var i = $sections.length - 1; i >= 0; i--) {
 				
 				$section 		= $sections.eq(i);
-				sectionId 		= $section.data('sectionid');
+				sectionId 		= $section.attr('data-sectionid');
 				
 				// No section ID
 				if(!sectionId) continue;
 				
 				// Comments
 				comments 		= this._getSectionComments(sectionId);
-				totalComments	= comments.find(this.options.selectors.comment).length;
+				totalComments	= comments.length;
 				
 				// Comment total text
 				$commentTotal = $(this.options.selectors.commentTotal, $section);
 				
-				// Build the comment total elements if they dont exist
-				if(!$commentTotal.length) {
-					$commentTotal = $('<span />').addClass(this.options.classes.commentTotal).appendTo($section);
-				}
-				
 				// Update the text
-				$commentTotal.text(totalComments + " comments");
+				$commentTotal.text(totalComments);
 				
 				// Setup the event for when you click the 'open' trigger for this section
-				// For now, we're using the comment count
 				$commentTotal.bind('click', $.proxy(function(){
 					this.toggleSectionComments(sectionId);
 					return false;
 				},this));
 				
-				// Add the reply buttons
-				this._initCommentReplyElements(comments);
+				// The comment form for this section
+				form = this.form.clone();
+				form.attr('id','comment-form-'+sectionId);
+				
+				// Setup the reply links
+				this._setupReplyLinks(sectionId,comments);
 				
 				// Save the section
 				this.sections[sectionId] = {
@@ -109,184 +100,90 @@
 					commentTotal: totalComments,
 					
 					// List of comments for this section
-					comments: comments
+					comments: comments,
+					
+					// Have the comments been loaded into their section yet?
+					commentsLoaded: false,
+					
+					// The comment form for this section
+					form:form
 				};
+				
+				this._setFormData(sectionId,0);
 			}
-		},
-		
-		// Returns the comment count for comments without a section
-		_getGlobalCommentCount: function(){
-			return $(this.options.selectors.comment+"[data-sectionid=0]", this.originalComments).length;
 		},
 		
 		// Return a list of DOM elements that are the comments for a section
 		_getSectionComments: function(sectionId) {
 			var commentList = this.originalComments.clone(true);
 			commentList.find(this.options.selectors.comment+"[data-sectionId!="+sectionId+"]").remove();
-			return commentList;
+			return commentList.find('.comment');
 		},
 		
-		// Update the hidden field in a form
+		// Update the hidden field in a form for a section
 		_setFormData: function(sectionId, parentId){
 			
 			var selectors 		= this.options.selectors,
-				form 			= $(selectors.commentForm, this.sections[sectionId].comments),
+				section			= this.sections[sectionId],
+				form 			= section.form,
 				sectionField 	= form.find(selectors.sectionIdInput),
 				parentField 	= form.find(selectors.parentCommentIdInput);
-				
+
 			sectionField.val(sectionId);
-			
-			if(parentId !== undefined) {
-				parentField.val(parentId);
-			}
-		},
-		
-		// Set the section ID for the reply links
-		_setReplySection: function(sectionId){
-			this.sections[sectionId].comments.find(this.options.classes).data({
-				'sectionId': parseInt(sectionId,10)
-			});
+			parentField.val(parentId);
+
+			return true;
 		},
 
 		// Load the comments for the section
 		loadSectionComments: function(sectionId) {
-			
 			var section		= this.sections[sectionId],
-				$section 	= section.element,
-				comments 	= this.sections[sectionId].comments;
+				comments 	= section.element.find('.comment-index');
 			
-			// Update the form and reply links
-			this._setFormData(sectionId,0);
-			this._setReplySection(sectionId);
-						
-			// append the new comments to the section
-			$section.append(comments);
+			comments.append(section.comments);
+			comments.after(section.form);
 
-			// Fire an event
-			this._trigger('loadsectioncomments', {}, 
-				$.extend(this._uiHash(), {
-					comments: comments,
-					sectionId: sectionId
-				})
-			);
+			return true;
 		},
 
-		// Toggle the section comments
-		// Load them if we have to first
+		// Loads the comments into the section
 		toggleSectionComments: function(sectionId) {
+			var section = this.sections[sectionId];
 			
-			var comments = this.sections[sectionId].comments,
-				openClass;
-			
-			if(comments.hasClass(openClass) === false) {
+			if(section.commentsLoaded === false) {
 				this.loadSectionComments(sectionId);
-				comments.addClass(openClass);
+				section.commentsLoaded = true;
 			}
-			else {
-				comments.remove().removeClass(openClass);
-			}
-		},
-		
-		// Creates a new reply button for a comment
-		commentReplyTrigger: function(sectionId){
-			var classes = this.options.classes,
-				$trigger = $('<a />')
-					.attr({ href: "#", 'data-section-id': sectionId })
-					.addClass(classes.commentReplyTrigger)
-					.text("Reply");
-			return $trigger;
+			
+			return true;
 		},
 
-		// For each comment create a reply link scoped to a group of comments
-		_initCommentReplyElements: function(comments){
-			//console.log('_createCommentReplyElements NSM.sectionComments');
-
-			var self = this,
-				options = this.options,
-				classes = this.options.classes,
-				selectors = this.options.selectors,
-				$commentEls = $(this.options.selectors.comment, comments);
+		// Setup the events for the reply links for each comment
+		_setupReplyLinks: function(sectionId,comments){
+			
+			var $replyLinks = $(this.options.selectors.commentReplyTrigger, comments);
+			
+			// When these are clicked, fire an event
+			$replyLinks.bind('click', $.proxy(function(event){
 				
-			for (var i = $commentEls.length - 1; i >= 0; i--){
-
-				var $comment 	= $commentEls.eq(i),
-					commentId 	= $comment.data('commentId') || 0,
-					sectionId 	= $comment.data('sectionId') || 0,
-					$trigger 	= $(selectors.commentReplyTrigger, $comment);
-				
-				// It doesn't exist
-				if(!$trigger.length) {
-					$trigger = this.commentReplyTrigger(sectionId);
-					$comment.prepend($trigger);
-					$trigger.bind('click', $.proxy(this._commentOnComment,this));
-				}
-
-				// $trigger.data({
-				// 					'commentId': commentId,
-				// 					'comments': $comments,
-				// 					'comment' : $comment,
-				// 					'sectionId': sectionId
-				// 				});
-			};
-		},
-
-		// Comment on a comment
-		_commentOnComment: function(event) {
-	
-			var $eventTarget = $(event.target),
-				$comment = $eventTarget.data("comment"),
-				commentId = $comment.data("commentid") || 0,
-				sectionId = $comment.data("sectionid") || 0,
-				$comments = $eventTarget.data("comments");
+				var target 		= $(event.target),
+					commentId 	= target.attr('data-commentId');
 						
-			$comments.find(this.options.selectors.parentCommentIdInput).val(commentId);
-			$comments.find(this.options.selectors.sectionIdInput).val(sectionId);
-
-			this._trigger('commentreply', event, 
-				$.extend(this._uiHash(), {
-					commentId: commentId,
-					comments: $comments,
-					comment: $comment,
-					sectionId: sectionId
-				})
-			);
-
-		},
-
-		// Sets a value of the options.
-		_setOption: function(key, value) {
-
-			var oldValue = this.options[key] || undefined;
-
-			// Call the base _setOption method
-			$.Widget.prototype._setOption.apply(this, arguments);
-
-			// The widget factory doesn't fire an callback for options changes by default
-			// In order to allow the user to respond, fire our own callback
-			this._trigger("setOption", { type: "setOption" }, {
-				option: key,
-				original: oldValue,
-				current: value
-			});
+				this._trigger('commentreply', event, {
+					widget:this,
+					sectionId:sectionId,
+					commentId:commentId,
+					target:target
+				});
+	
+			},this));
+			
+			return true;
 		},
 
 		// Use the destroy method to reverse everything your plugin has applied
 		destroy: function() {
-			//console.log('_destroy NSM.sectionComments');
-
-			// 1. Remove any new elements that you created
-			// 2. Unbind any events that may still exist
-			// 3. Remove any classes, including CSS framework classes, that you applied
-			// After you're done, you still need to invoke the "base" destroy method
-			// Does nice things like unbind all namespaced events on the original element
 			$.Widget.prototype.destroy.call(this);
-		},
-
-		_uiHash: function(inst) {
-			var self = inst || this;
-			return {
-				sections: self.sections
-			};
 		}
 
 	});
